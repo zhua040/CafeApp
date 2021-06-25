5eb7eb76c4c3b9cbdaead347b86e72c4a0fe15b2
Joseph <chaofengjojo@hotmail.com>
2021-06-25 20:43:25 +1200

    Food update2

---
+++ app/src/main/java/com/example/littlekingsapp/foods/Food_MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.littlekingsapp.foods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.littlekingsapp.R;
+
+public class Food_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_main_activity_layout);
+    }
+}
