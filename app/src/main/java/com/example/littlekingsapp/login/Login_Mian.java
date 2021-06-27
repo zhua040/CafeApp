04f0bed64f339ba6d7414a349614890c689ce5a3
Joseph <chaofengjojo@hotmail.com>
2021-06-28 00:24:26 +1200

    login xml

---
+++ app/src/main/java/com/example/littlekingsapp/login/Login_Mian.java
@@ -0,0 +1,16 @@
+package com.example.littlekingsapp.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.littlekingsapp.R;
+
+public class Login_Mian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main_layout);
+    }
+}
