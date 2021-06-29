3309837b8315175a3bd157de9bc8c7dfb070abd9
Joseph <chaofengjojo@hotmail.com>
2021-06-29 13:19:57 +1200

    Basic google map

--- app/src/main/java/com/example/littlekingsapp/MainActivity.java
+++ app/src/main/java/com/example/littlekingsapp/MainActivity.java
@@ -7,13 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.littlekingsapp.Map.MapsActivity_Main;
 import com.example.littlekingsapp.drinks.Drink_MainAtivity;
 import com.example.littlekingsapp.foods.Food_MainActivity;
 import com.example.littlekingsapp.login.Login_Mian;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CardView CardDrinks, CardFoods;
+    private CardView CardDrinks, CardFoods, CardMap;
 
     String shortName;
 
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         //Casting CardView
         CardDrinks = (CardView) findViewById(R.id.card_Drinks);
         CardFoods = (CardView) findViewById(R.id.card_Foods);
+        CardMap = (CardView) findViewById(R.id.card_map);
 
 
         //Set on_clickingListener for each card
@@ -45,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Food_MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        CardMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity_Main.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/littlekingsapp/Map/MapsActivity_Main.java
@@ -0,0 +1,40 @@
+package com.example.littlekingsapp.Map;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.example.littlekingsapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity_Main extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_layout);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng littleKing = new LatLng(-36.77301, 174.76673);
+        mMap.addMarker(new MarkerOptions().position(littleKing).title("132,Kitchener Road Auckland 0620"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(littleKing));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(littleKing, 12.0f));
+    }
+}
